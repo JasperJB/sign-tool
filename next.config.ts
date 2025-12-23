1c492c09733c3230b001c31d4e31937d2797782d
JasperJB <jasperbayless@gmail.com>
2025-12-23 11:16:51 -0800

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // This suppresses a warning about the 'canvas' dependency
+    // which is not needed in the browser environment.
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
